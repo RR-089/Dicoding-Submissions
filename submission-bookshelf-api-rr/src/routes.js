7649c80d9c9d2a0580185fe1d2d95e19e91fd3fe
U C O K G G <85013630+RR-089@users.noreply.github.com>
2023-12-31 20:15:51 +0700

    Add files via upload

---
+++ submission-bookshelf-api-rr/src/routes.js
@@ -0,0 +1,62 @@
+const {
+  addBookHandler, getAllBooksHandler,
+  getBookByIdHandler, editBookByIdHandler,
+  deleteBookByIdHandler
+} = require('./handler')
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return 'Homepage'
+    }
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: () => {
+      return 'Halaman tidak dapat diakses dengan method tersebut'
+    }
+  },
+
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => {
+      return 'Halaman tidak ditemukan'
+    }
+  },
+
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler
+  },
+
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler
+  },
+
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler
+  },
+
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler
+  },
+
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler
+  }
+]
+
+module.exports = routes
